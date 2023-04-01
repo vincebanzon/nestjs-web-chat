472b4bb23ed64ca5c98506edd2968e54c1f099e6
Vince Banzon <vincebanzon@gmail.com>
2023-04-01 18:42:06 +0400

    Working chat

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
 
 @Injectable()
 export class MessagesService {
-  create(createMessageDto: CreateMessageDto) {
-    return 'This action adds a new message';
+  message: Message[] = [{name: "vince", message: 'here'}]
+  clientToUser = {};
+
+  identify(name: string, clientId: string) {
+    this.clientToUser[clientId] = name;
+    return Object.values(this.clientToUser);
+  }
+
+  getClientName(clientId: string) {
+    return this.clientToUser[clientId];
+  }
+
+  create(createMessageDto: CreateMessageDto, clientId: string) {
+    const message = {
+      name: this.clientToUser[clientId],
+      message: createMessageDto.message
+    }
+
+    this.message.push(message);
+    return message;
   }
 
   findAll() {
-    return `This action returns all messages`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} message`;
-  }
-
-  update(id: number, updateMessageDto: UpdateMessageDto) {
-    return `This action updates a #${id} message`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} message`;
+    return this.message;
   }
 }
